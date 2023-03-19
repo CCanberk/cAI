4f946b1b75e20ae8e74ed05bf77a7d2d774d8d7e
Canberk Çevik <cevik.canberk@proton.me>
2023-03-20 00:37:33 +0300

    privacy issues

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import openai
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
